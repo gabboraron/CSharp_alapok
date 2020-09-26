e7770b6b599d02f2fb7e56c73d8fc7fc366fd918
gabboraron <gabboraron@gmail.com>
2020-09-26 21:12:31 +0200

    Fizetés számláló program
    Egy if-then-else alapú fizetés számoló program.

---
+++ payCheck/payCheck/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace payCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hoursWorked = 0;
+            double payRate = 1300;
+
+            Console.WriteLine("how many hours did you work this week?");
+
+            //hoursWorked = Convert.ToDouble(Console.ReadLine());
+            if (double.TryParse(Console.ReadLine(), out hoursWorked))
+            {
+                if (hoursWorked > 0)
+                {
+                    //calc payment
+                    if (hoursWorked > 40)
+                    {
+                        //calc overtime
+                        double payAmount = hoursWorked * payRate + (hoursWorked - 40) * payRate * 1.5;
+                        Console.WriteLine("Your payment is: " + payAmount);
+                    }
+                    else
+                    {
+                        double payAmount = hoursWorked * payRate;
+                        Console.WriteLine("Your payment is: " + payAmount);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No work = no money");
+                }
+            }
+            else
+            {
+                Console.WriteLine("BAD DATA FORMAT!");
+            }
+        }
+    }
+}
