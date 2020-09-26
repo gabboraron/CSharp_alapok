0e129984cadc0d1ba10e70c7a12b6c1da883b82b
gabboraron <gabboraron@gmail.com>
2020-09-26 17:55:35 +0200

    Console I/O

---
+++ ConsoleBasedIO/ConsoleBasedIO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleBasedIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What  is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, nice name!"); //fontos, hogy " és nem '
+            Console.Write($"so, hey {name}, What would you like to order today? ");
+            var order = Console.ReadLine();
+            Console.Write($"Well, this {order} sounds delicious!\nThis will be ");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("$5.6");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
