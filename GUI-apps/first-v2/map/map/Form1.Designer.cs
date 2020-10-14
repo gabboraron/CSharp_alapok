5f49fb9c2404b947b5fdcfad192493c81f8af3ab
gabboraron <gabboraron@gmail.com>
2020-10-15 00:23:40 +0200

    térkép

---
+++ GUI-apps/first-v2/map/map/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace map
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.country = new System.Windows.Forms.TextBox();
+            this.city = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.citylbl = new System.Windows.Forms.Label();
+            this.streetlbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.street = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.country);
+            this.splitContainer1.Panel1.Controls.Add(this.city);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.citylbl);
+            this.splitContainer1.Panel1.Controls.Add(this.streetlbl);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.street);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(1184, 552);
+            this.splitContainer1.SplitterDistance = 299;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // country
+            // 
+            this.country.Location = new System.Drawing.Point(8, 294);
+            this.country.Multiline = true;
+            this.country.Name = "country";
+            this.country.Size = new System.Drawing.Size(274, 44);
+            this.country.TabIndex = 12;
+            // 
+            // city
+            // 
+            this.city.Location = new System.Drawing.Point(10, 185);
+            this.city.Multiline = true;
+            this.city.Name = "city";
+            this.city.Size = new System.Drawing.Size(274, 44);
+            this.city.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 39);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ország";
+            // 
+            // citylbl
+            // 
+            this.citylbl.AutoSize = true;
+            this.citylbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.citylbl.Location = new System.Drawing.Point(5, 143);
+            this.citylbl.Name = "citylbl";
+            this.citylbl.Size = new System.Drawing.Size(106, 39);
+            this.citylbl.TabIndex = 7;
+            this.citylbl.Text = "Város";
+            // 
+            // streetlbl
+            // 
+            this.streetlbl.AutoSize = true;
+            this.streetlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.streetlbl.Location = new System.Drawing.Point(3, 39);
+            this.streetlbl.Name = "streetlbl";
+            this.streetlbl.Size = new System.Drawing.Size(87, 39);
+            this.streetlbl.TabIndex = 6;
+            this.streetlbl.Text = "Utca";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(42, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 63);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Keresés";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // street
+            // 
+            this.street.Location = new System.Drawing.Point(10, 81);
+            this.street.Multiline = true;
+            this.street.Name = "street";
+            this.street.Size = new System.Drawing.Size(274, 44);
+            this.street.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(881, 552);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 552);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Térkép";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label citylbl;
+        private System.Windows.Forms.Label streetlbl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox street;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox country;
+        private System.Windows.Forms.TextBox city;
+    }
+}
+
---
+++ GUI-apps/first-v2/map/map/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace map
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder querryData = new StringBuilder();
+                querryData.Append("https://maps.google.com/maps?q=");
+
+
+                if (city.Text != string.Empty)
+                {
+                    querryData.Append(Convert.ToString(city.Text) + ",+");
+                }
+
+                if (street.Text != string.Empty)
+                {
+                    querryData.Append(Convert.ToString(street.Text) + ",+");
+                }
+
+               /* if (zipnumber.Text != string.Empty)
+                {
+                    querryData.Append(Convert.ToString(zipnumber.Text) + ",+");
+                }*/
+
+                if (country.Text != string.Empty)
+                {
+                    querryData.Append(Convert.ToString(country.Text) + ",+");
+                }
+                webBrowser1.ScriptErrorsSuppressed = true;
+                webBrowser1.Navigate(querryData.ToString());
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message.ToString(), "ERROR");
+                throw;
+            }
+        }
+    }
+}
