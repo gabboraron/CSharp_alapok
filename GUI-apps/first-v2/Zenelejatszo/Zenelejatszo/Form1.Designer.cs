0bbcb51ece8ab9991022b1f8c15e28b86e597a91
gabboraron <gabboraron@gmail.com>
2020-10-16 23:54:47 +0200

    zene lejátszó program a windows music player használatával

---
+++ GUI-apps/first-v2/Zenelejatszo/Zenelejatszo/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Zenelejatszo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pathLbl = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.playBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(646, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(2, 1);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(691, 564);
+            this.player.TabIndex = 1;
+            // 
+            // pathLbl
+            // 
+            this.pathLbl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pathLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pathLbl.ForeColor = System.Drawing.Color.White;
+            this.pathLbl.Location = new System.Drawing.Point(217, 529);
+            this.pathLbl.Multiline = true;
+            this.pathLbl.Name = "pathLbl";
+            this.pathLbl.Size = new System.Drawing.Size(476, 36);
+            this.pathLbl.TabIndex = 2;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.browseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseBtn.ForeColor = System.Drawing.Color.White;
+            this.browseBtn.Location = new System.Drawing.Point(2, 529);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(216, 36);
+            this.browseBtn.TabIndex = 3;
+            this.browseBtn.Text = "Dal kiválasztása...";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // playBtn
+            // 
+            this.playBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.playBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playBtn.ForeColor = System.Drawing.Color.White;
+            this.playBtn.Location = new System.Drawing.Point(2, 1);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(135, 36);
+            this.playBtn.TabIndex = 4;
+            this.playBtn.Text = "lejátszás";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // pauseBtn
+            // 
+            this.pauseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.pauseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseBtn.ForeColor = System.Drawing.Color.White;
+            this.pauseBtn.Location = new System.Drawing.Point(2, 43);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(135, 36);
+            this.pauseBtn.TabIndex = 5;
+            this.pauseBtn.Text = "megállítás";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.stopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopBtn.ForeColor = System.Drawing.Color.White;
+            this.stopBtn.Location = new System.Drawing.Point(2, 85);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(135, 36);
+            this.stopBtn.TabIndex = 6;
+            this.stopBtn.Text = "kikapcsolás";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(693, 567);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.playBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.pathLbl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zenelejátszó";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.TextBox pathLbl;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
